a0317449ad23e0288cfeb0bfa1e5d33948c5a33c
gsemir0418 <845217811@qq.com>
2024-05-21 09:17:58 +0800

    refactor: database.TruncateTables

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,8 +3,8 @@ package database
 import (
 	"log"
 	"os"
+	"testing"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -16,10 +16,8 @@ func ConnectDB() {
 		return
 	}
 	// 加载 .env 文件
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	// 因为测试环境的工作目录与你的 .env 文件所在的目录不同导致的。
+	// godotenv.Load() 默认会在当前工作目录下查找 .env 文件。如果你的测试运行在一个不同的目录，需要确保正确地指定 .env 文件的路径。
 
 	// 从环境变量中获取数据库连接字符串
 	dsn := os.Getenv("DB_DSN")
@@ -43,5 +41,26 @@ func Migrate() {
 		if err != nil {
 			log.Fatal("Fail to migrate database!", err)
 		}
+	}
+}
+
+func TruncateTables(t *testing.T, tables []string) {
+	// 禁用外键检查
+	err := DB.Exec("SET FOREIGN_KEY_CHECKS=0;").Error
+	if err != nil {
+		t.Fatalf("Failed to disable foreign key checks: %v", err)
+	}
+
+	// 清空所有给定的表
+	for _, table := range tables {
+		if err = DB.Exec("TRUNCATE TABLE " + table + ";").Error; err != nil {
+			t.Fatalf("Failed to truncate table %s: %v", table, err)
+		}
+	}
+
+	// 重新启用外键检查
+	err = DB.Exec("SET FOREIGN_KEY_CHECKS=1;").Error
+	if err != nil {
+		t.Fatalf("Failed to enable foreign key checks: %v", err)
 	}
 }
--- test/setup_test.go
+++ test/setup_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"account-app-gin/config"
 	"account-app-gin/internal/database"
 	"context"
 	"testing"
@@ -14,18 +15,16 @@ var (
 )
 
 func setUpTestCase(t *testing.T) {
+	// 加载环境变量
+	config.LoadEnv()
 	// 连接数据库
 	database.ConnectDB()
 
 	r = gin.New()
 	// 默认上下文
 	c = context.Background()
-	// 清空 User 表
-	if err := database.DB.Migrator().DropTable(&database.User{}); err != nil {
-		t.Fatal(err)
-	}
-	// 清空 Items 表
-	if err := database.DB.Migrator().DropTable(&database.User{}); err != nil {
-		t.Fatal(err)
-	}
+	// 清空 User 表 TRUNCATE 相当于先 DROP 在 CREATE
+	// 存在外键约束的话，为了保证数据完整性，不能使用 TRUNCATE
+	// 使用 DELETE 性能很差，所以这里使用更激进的策略，先关闭外键约束检查，清空后再恢复
+	database.TruncateTables(t, []string{"users", "items"})
 }
