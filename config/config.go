1a50a36c4017327f12c9916549ff4ba889af2a61
gsemir0418 <845217811@qq.com>
2024-05-24 09:16:00 +0800

    chore: load .env according to GIN_ENV

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 	"path/filepath"
 	"runtime"
 
@@ -9,13 +10,21 @@ import (
 )
 
 func LoadEnv() {
-	// 获取当前文件的路径
+	// 获取当前文件的绝对路径
 	_, b, _, _ := runtime.Caller(0)
 	basepath := filepath.Dir(b)
 
-	// 加载距离当前文件上级的 .env 文件
-	err := godotenv.Load(filepath.Join(basepath, "..", ".env"))
+	// 获取环境变量 GIN_ENV，默认值为 "dev"
+	env := os.Getenv("GIN_ENV")
+	if env == "" {
+		env = "dev"
+	}
+
+	// 确定要加载的 .env 文件
+	envFile := filepath.Join(basepath, "..", ".env."+env)
+	// 加载环境文件
+	err := godotenv.Load(envFile)
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Fatalf("Error loading .env file for environment %s: %v", env, err)
 	}
 }
